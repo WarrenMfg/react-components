f326e45ef00313972995e78548717bb8de373b5d
WarrenMfg <warrenmfg@icloud.com>
2019-12-30 14:54:36 -0500

    completed exercise

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // TODO
+class GroceryList extends React.Component {
+
+  render() {
+    return (
+      <ul>
+        {['Eggs', 'Milk', 'Bread'].map(item => <GroceryItem item={item} />)}
+      </ul>
+    );
+  }
+
+}
+
+class GroceryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addedToCart: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this);
+    this.handleOnMouseOut = this.handleOnMouseOut.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({addedToCart: !this.state.addedToCart});
+  }
+
+  handleOnMouseEnter(e) {
+    e.target.style.fontWeight = 'bold';
+    e.target.style.cursor = 'pointer';
+  }
+
+  handleOnMouseOut(e) {
+    e.target.style.fontWeight = '';
+  }
+
+  render() {
+    let style = {textDecoration: this.state.addedToCart ? 'line-through' : 'none'};
+
+    return <li style={style} onClick={this.handleClick} onMouseEnter={this.handleOnMouseEnter} onMouseOut={this.handleOnMouseOut}>{this.props.item}</li>
+  }
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
